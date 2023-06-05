0303fcdf05efb8535ef58f3859190eeb12f45caa
wisdomm <00313210@rijnijssel.nl>
2023-06-05 13:25:41 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import Root from "./routes/root";
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-    },
-]);
-
-ReactDOM.createRoot(document.getElementById("sidebar")).render(
-    <React.StrictMode>
-        <RouterProvider router={router}/>
-    </React.StrictMode>
-)
 
 function App() {
     return(
         <div>
-            <router />
+            <h2>heoi</h2>
         </div>
     )
 }
